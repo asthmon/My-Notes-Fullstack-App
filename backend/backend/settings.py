b01beb38f40772ad1067c2809caefdbaa1b94a69
Heryza . <heryzariz15@gmail.com>
2025-12-26 18:22:29 +0800

    local database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,7 +98,10 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
+    'default' :{
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3'
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
